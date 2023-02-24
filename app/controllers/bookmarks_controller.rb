e96a3d2712f65dae424aac140cf51ec739e6caf4
Jeffrey Boateng <jeff05@gmx.net>
2023-02-24 12:01:22 +0100

    Bookmarks Controller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,25 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  # def create
+  #   @bookmark = Bookmark.new(bookmark_params)
+  #   @bookmark.save
+  # end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list_id = params[:list_id]
+    @bookmark.save
+    redirect_to list_path(@bookmark.list)
+    raise
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
